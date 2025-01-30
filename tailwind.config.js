ac8694e4bcf0bfc382fd35312358a2bec7cd4a50
faisalbari193 <faisalbari193@gmail.com>
2025-01-31 01:33:24 +0600

    add few data

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          typing: "typing 3s steps(25) infinite",
+        },
+          keyframes: {
+            typing: {
+              "0%": { width: "0%" },
+              "100%": { width: "100%" },
+            },
+      },
     },
     plugins: [],
   }
